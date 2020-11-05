b2152a1342008e32fcee4c4cfc64320601a37a45
milovanderlinden <milo@dogodigi.net>
2020-11-05 17:53:04 +0100

    Trying EditText

--- src/components/MarketListItem.tsx
+++ src/components/MarketListItem.tsx
@@ -3,6 +3,7 @@ import { Component } from "react"
 import { Market } from "../models"
 import { Link } from "react-router-dom"
 import { Descriptions, Tag } from "antd"
+import EditText from "react-edit-text"
 
 export default class MarketListItem extends Component<{ market: Market, marketId: string }> {
     render() {
@@ -13,7 +14,11 @@ export default class MarketListItem extends Component<{ market: Market, marketId
             column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
         >
             <Descriptions.Item label="ID"><b>{marketId}</b></Descriptions.Item>
-            <Descriptions.Item label="Markt"><em>{market.name || "onbekend"}</em></Descriptions.Item>
+            <Descriptions.Item label="Markt"><EditText 
+                id={`market_${marketId}_name`}
+                placeholder="klik hier om een naam in te vullen"
+                value={market.name} />
+                </Descriptions.Item>
             <Descriptions.Item label="Fase"><Tag className={market.phase || "onbekend"}>{market.phase || "onbekend"}</Tag></Descriptions.Item>
             <Descriptions.Item label="Dagen">
 
