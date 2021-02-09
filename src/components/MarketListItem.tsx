c2b22890d5ea7bff9381bb542f44c5629bc6c6f0
milovanderlinden <milo@dogodigi.net>
2021-02-09 09:53:54 +0100

    Styling fix

--- src/components/MarketListItem.tsx
+++ src/components/MarketListItem.tsx
@@ -28,7 +28,7 @@ export default class MarketListItem extends Component<{ market: Market, marketId
                 <h2 style={{ fontWeight: "bold", fontSize: "1.2em" }}>{marketId} </h2>
             </Link>
             <p style={{ color: "rgba(0, 0, 0, 0.45)" }}> {this.state.market.name || ""}</p>
-            <p> {this.state.market.phase || "."}</p>
+            <p><span  style={{ color: "rgba(0, 0, 0, 0.45)" }}>fase:</span> {this.state.market.phase || " "}</p>
             {/* {this.state.market && <>{Object.keys(this.state.market.events).map((key: string) => {
                     if(this.state.market.events[key].weekday){
                         return <div key={key}>{key}</div>
